4e41700a6b1487db055140430259c5a800e89466
Muhammed junaid <mdjunaiduh4@gmail.com>
2026-02-15 16:12:46 +0530

    Add files via upload

---
+++ DSA-java/DSA-java/src/basic/array/ArraySum.java
@@ -0,0 +1,26 @@
+package dsa.basic.array;
+
+import java.util.Scanner;
+
+public class ArraySum {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a size");
+        int size = sc.nextInt();
+
+        int arr[] = new int[size];
+
+        int sum = 0;
+
+        System.out.println("Enter  elements");
+        for(int i=0;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i=0;i<arr.length;i++){
+            sum = sum + arr[i];
+        }
+        System.out.println("The of an array is : "+sum);
+        sc.close();
+    }
+}
+
