bc269480441cdd6c0055ac185b490db8298538c4
Muhammed junaid <mdjunaiduh4@gmail.com>
2026-02-16 15:14:01 +0530

    Add files via upload

---
+++ DSA-java/DSA-java/src/string/PrintLength.java
@@ -0,0 +1,16 @@
+package dsa.basic.string;
+
+import java.util.Scanner;
+
+public class PrintLength {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a String : ");
+        String str = sc.nextLine();
+
+        System.out.println(str.length());
+        sc.close();
+    }
+
+}
