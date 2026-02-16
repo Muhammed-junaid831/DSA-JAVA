9853db9e5244018eb7e7f6953692f31b90ce7e31
Muhammed junaid <mdjunaiduh4@gmail.com>
2026-02-16 14:44:37 +0530

    Add files via upload

---
+++ DSA-java/DSA-java/src/basic/array/ArrayGreater.java
@@ -0,0 +1,28 @@
+package dsa.basic.array;
+
+import java.util.Scanner;
+
+public class ArrayGreater {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a size");
+        int size = sc.nextInt();
+
+        int arr[] = new int[size];
+
+
+        System.out.println("Enter  elements");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter K : ");
+        int k = sc.nextInt();
+
+        for (int i = 0; i < size; i++) {
+            if (arr[i] > k) {
+                System.out.println(arr[i]);
+            }
+        }
+        sc.close();
+    }
+}
